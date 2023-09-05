09b270da93958ad8ae6d710a7b8d9018daf9754a
cassiocoltri <cassiocoltri@gmail.com>
2023-09-05 16:32:41 -0300

    Implementacao do Scanner e do Locale.US

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,8 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class ContaTerminal {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println("Olar Mundo grande!");
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int numero;
+		String agencia;
+		String nomeCliente;
+		Double saldo;
+		
+		sc.close();
 
 	}
 
